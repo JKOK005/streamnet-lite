5187ac4eccb28f6f72cc533d8e9b35d3da3cfb8b
Johan Kok <johan.kok@grabtaxi.com>
2021-04-29 12:14:26 +0800

    config changes

--- actor/Executor.py
+++ actor/Executor.py
@@ -41,19 +41,9 @@ class StreamnetExecutor(pykka.ThreadingActor):
 		elif type(message) is messages.BackpropStreamlet:
 			tensor 			= message.get_tensor()
 			self.logger.debug("Received tensor of shape: {0}".format(tensor.shape))
-			
-			import time
-			start = time.time()
 			bp_tensor 		= self.model.backprop_pass(input_tensor = self.cur_input, dl_dy = tensor)
-			self.logger.info("Backprop loss: {0}".format(time.time() - start))	
-
-			start = time.time()
 			wupdt_tensor	= self.model.delta_weights(input_tensor = self.cur_input, dl_dy = tensor)
-			self.logger.info("Backprop weights: {0}".format(time.time() - start))	
-
-			start = time.time()
 			bupdt_tensor	= self.model.delta_bias(input_tensor = self.cur_input, dl_dy = tensor)
-			self.logger.info("Backprop bias: {0}".format(time.time() - start))	
 
 			bp_stream 		= BackpropStreamlet(tensor = bp_tensor, fragments = message.get_frag(), index = message.get_index())
 			wupdt_stream 	= WeightStreamlet(tensor = wupdt_tensor, fragments = message.get_frag(), index = message.get_index())
