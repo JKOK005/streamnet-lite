0a1f5a50c026f07d4d2cd887f385083cd2bb6c95
Johan Kok <johan.kok@grabtaxi.com>
2021-04-29 14:19:28 +0800

    config changes

--- main.py
+++ main.py
@@ -62,12 +62,12 @@ if __name__ == "__main__":
 	
 	layers 	= [
 		build_conv(filters = 16, kernel = (3,3), strides = (1,1), activation = 'relu', num_routees = 8),
-		build_conv(filters = 8, kernel = (3,3), strides = (1,1), activation = 'relu', num_routees = 8),
-		build_conv(filters = 4, kernel = (3,3), strides = (1,1), activation = 'relu', num_routees = 8),
+		build_conv(filters = 8, kernel = (3,3), strides = (1,1), activation = 'relu', num_routees = 4),
+		build_conv(filters = 4, kernel = (3,3), strides = (1,1), activation = 'relu', num_routees = 4),
 		build_flatten(num_routees = 1),
 		build_dense(units = 16, activation = 'relu', num_routees = 8),
-		build_dense(units = 8, activation = 'relu', num_routees = 8),
-	  	build_dense(units = 4, activation = 'sigmoid', num_routees = 8)
+		build_dense(units = 8, activation = 'relu', num_routees = 4),
+	  	build_dense(units = 4, activation = 'sigmoid', num_routees = 4)
 	]
 
 	# Purely convolution layers
